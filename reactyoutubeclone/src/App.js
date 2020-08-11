dbae89c3d57bb5b4d2316917e8f43779d47f1395
unknown <larserlandson5@gmail.com>
2020-08-11 18:12:48 -0500

    Adding side bar/top 25 and axios template locations

--- reactyoutubeclone/src/App.js
+++ reactyoutubeclone/src/App.js
@@ -1,11 +1,24 @@
 import React from 'react';
+import "./App.css";
 import Header from "./Header.js";
+// import Sidebar from "./Sidebar.js";
+// import Sidebar from "./Top25Videos.js"
 
 function App() {
+  function fetchData(){
+    //makes axios request
+    //assign response data to prop
+  }
   return (
     <div className="Header">
-    <Header/>
-    </div>
+      <Header />
+      <div className="app__page">
+        <Sidebar />
+        <Top25Videos videosData={this.fetchData()}/>
+      </div>
+
+
+      </div>
   );
 }
 
---
+++ reactyoutubeclone/src/Top25Videso.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Top25Videso() {
+    return (
+        <div>
+            <h2>video list </h2>
+        </div>
+    )
+}
+
+export default Top25Videso
