8162915d27a404da3d5fb47523017aa5c156f103
unknown <larserlandson5@gmail.com>
2020-08-12 20:33:19 -0500

    started like button

--- reactyoutubeclone/src/App.js
+++ reactyoutubeclone/src/App.js
@@ -6,47 +6,83 @@ import Top25Videos from "./Top25Videso.js";
 import { Grid } from '@material-ui/core';
 import SearchBar from "./components/SearchBar.js";
 import VideoDetail from "./components/VideoDetail.js";
-import youtube from "./api/youtube.js";
+import youtube from "./api/youtube";
+import Checkbox from '@material-ui/core/Checkbox';
+
 
 
 class App extends React.Component {
-  
-    state = {
-      searchTerm:'',
-    }
+
+
+
   handleSubmit = async (searchTerm) => {
-    const response = await youtube.get('search',{
+    const response = await youtube.get('search', {
       params: {
-      part: 'snippet',
-      maxResults: 5,
-      key:"AIzaSyB_lXlpLKvWO4JXo2rQnGStK5ptBtnF7gA",
-      q:searchTerm,
- } 
-});
+        part: 'snippet',
+        maxResults: 5,
+        key: "AIzaSyB_lXlpLKvWO4JXo2rQnGStK5ptBtnF7gA",
+        q: searchTerm,
+      }
+    });
+
     console.log(response.data.items);
-     this.setState({videos: response.data.items, selectedVideo: response.data.items[0]});
+    this.setState({ videos: response.data.items, selectedVideo: response.data.items[0] });
   }
 
-render(){
-  return (
+  render() {
+    return (
 
-    <Grid justify="center" container spacing={10}>
-      <Grid item xs={5}>
-        <Grid container spacing={10}>
-          <Grid item xs={5}>
-            <SearchBar onFormSubmit={this.handleSubmit}></SearchBar>
-          </Grid>
-          <Grid item xs={8}>
-            <VideoDetail></VideoDetail>
-          </Grid>
-          <Grid item xs={4}>
+      <Grid justify="center" container spacing={16}>
+        <Grid item xs={10}>
+          <Grid container spacing={10}>
+            <Grid item xs={5}>
+              <SearchBar onFormSubmit={this.handleSubmit}></SearchBar>
+            </Grid>
+            <Grid item xs={8}>
+              <VideoDetail></VideoDetail>
+            </Grid>
+            <Grid item xs={4}>
+            </Grid>
           </Grid>
         </Grid>
       </Grid>
-    </Grid>
+      <div>
+        <Checkbox
+          checked={checked}
+          onChange={handleChange}
+          inputProps={{ 'aria-label': 'primary checkbox' }}
+        />
+        <Checkbox
+          defaultChecked
+          color="primary"
+          inputProps={{ 'aria-label': 'secondary checkbox' }}
+        />
+        <Checkbox inputProps={{ 'aria-label': 'uncontrolled-checkbox' }} />
+        <Checkbox disabled inputProps={{ 'aria-label': 'disabled checkbox' }} />
+        <Checkbox disabled checked inputProps={{ 'aria-label': 'disabled checked checkbox' }} />
+        <Checkbox
+          defaultChecked
+          indeterminate
+          inputProps={{ 'aria-label': 'indeterminate checkbox' }}
+        />
+        <Checkbox
+          defaultChecked
+          color="default"
+          inputProps={{ 'aria-label': 'checkbox with default color' }}
+        />
+        <Checkbox
+          defaultChecked
+          size="small"
+          inputProps={{ 'aria-label': 'checkbox with small size' }}
+        />
+      </div>
 
- )
-}
+ 
+
+ 
+    
+)
+  }
 
 }
 export default App;
--- reactyoutubeclone/src/components/SearchBar.js
+++ reactyoutubeclone/src/components/SearchBar.js
@@ -4,24 +4,30 @@ import { Paper, TextField } from '@material-ui/core';
 class SearchBar extends React.Component {
     
         state = {
-            searchTerm: "",
+            searchTerm: '',
         }
+
         handleChange = (event) => {
             console.log(event.target.value);
-            this.setState({ searchTerm: event.taget.value });
+
+            this.setState({ searchTerm: event.target.value });
+         
         }
+
         handleSubmit = (event) => {
             const { searchTerm } = this.state;
             const { onFormSubmit } = this.props;
             onFormSubmit(searchTerm);
+       
+            event.preventdefault();
             console.log(searchTerm)
-
-            // event.target.preventDefault();
+          
         }
 
         render() {
+           
             return (
-                <Paper elevation={5} style={{ padding: '25px' }}>
+                <Paper elevation={6} style={{ padding: '25px' }}>
                     <form onSubmit={this.handleSubmit}>
                         <TextField fullWidth label="Search-->" onChange={this.handleChange}></TextField>
                     </form>
